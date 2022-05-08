1ec882898c58f80cbf4854d5f08754f5f935c0f5
ArianPaymozd <70002472+ArianPaymozd@users.noreply.github.com>
2022-05-07 20:50:55 -0700

    first deploy

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const express = require("express")
 const app = express()
 const cors = require("cors")
+require("dotenv").config
 
 app.use(express.json())
 app.use(cors())
+app.use((error, req, res, next) => {
+    let response
+    if (process.env.NODE_ENV === 'production') {
+      response = { error: { message: 'server error' }}
+    } else {
+      response = { error }
+    }
+    res.status(500).json(response)
+  })
 
 app.use("/auth", require("./routes/jwtAuth"))
 app.use("/home", require("./routes/homeRoutes"))
 app.use("/chore", require("./routes/choreRoutes"))
-
+const PORT = precess.env.PORT || 5000
 app.listen(5000, () => {
     console.log('server has')
 })
